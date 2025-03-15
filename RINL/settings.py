ef65d1d5d0605bee5094172580af965f1f99bafa
YSChandu <yarrapothusaichandu@gmail.com>
2025-03-15 15:44:03 +0530

    1

--- RINL/settings.py
+++ RINL/settings.py
@@ -123,15 +123,13 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure this matches your collectstatic path
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
